728c476969a7f399eced551f1a4442720197accd
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-11-09 03:51:06 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,6 +1,12 @@
 class Project
-  attr_reader(:title)
+  attr_reader(:title, :backer)
   def initialize(title)
     @title = title
+    @backer = []
+  end
+
+  
+  def back_backer(backer)
+    @backers << backer
   end
 end
